be299fd54c712ee947205d6ef2fc58478bc8688b
Alejandro Saez <alejandrosaezc@gmail.com>
2022-07-22 13:03:52 +0200

    List Page Done

---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { useState } from "react";
+import { Button, Card, Title, Paragraph, IconButton } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+
+const CardComponent = (props:any) => {
+  const [heart, setHeart] = useState("heart-outline");
+  const handleHeart = () => {
+    if (heart == "heart-outline") {
+      setHeart("heart");
+    } else {
+      setHeart("heart-outline");
+    }
+  };
+  return (
+    <Card elevation={5} style={{ marginVertical: 10, marginHorizontal: 10 }}>
+      <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+      <View
+        style={{
+          flexDirection: "row",
+          paddingVertical: 5,
+        }}
+      >
+        <Card.Content style={{ flex: 1, minWidth: 250 }}>
+          <Title>{props.title}</Title>
+          <Paragraph>{props.content}</Paragraph>
+        </Card.Content>
+        <Card.Actions style={{ flex: 2, justifyContent: "flex-end" }}>
+          <IconButton
+            size={20}
+            icon={heart}
+            color="red"
+            onPress={() => handleHeart()}
+          ></IconButton>
+          <IconButton
+            size={20}
+            icon="dots-vertical"
+            color="gray"
+            onPress={() =>
+              props.navigation.navigate("Detalles", {
+                title: props.title,
+                content: props.content,
+              })
+            }
+          ></IconButton>
+        </Card.Actions>
+      </View>
+    </Card>
+  );
+};
+
+export default CardComponent;
--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,10 +1,50 @@
 import React from "react";
-import { Text } from "react-native";
+import { FlatList, StyleSheet, Text, ScrollView } from "react-native";
+import { View } from "react-native";
+import { Navigation } from '../types';
+import CardComponent from "../components/CardComponent";
 
+const styles = StyleSheet.create({
+    contentContainer: {
+      paddingTop: 10,
+      paddingBottom: 120,
+    },
+  });
 
-export const ListScreen = () => {
+  type Props = {
+    navigation: Navigation;
+  };
+
+export const ListScreen = ({ navigation }: Props) => {
 
     return (
-        <Text>Hola Mundo</Text>
+        <View>
+      <FlatList
+        contentContainerStyle={styles.contentContainer}
+        data={[
+          { key: "Elemento 1", content: "Este es el contenido del elemento 1" },
+          { key: "Elemento 2", content: "Este es el contenido del elemento 2" },
+          { key: "Elemento 3", content: "Este es el contenido del elemento 3" },
+          { key: "Elemento 4", content: "Este es el contenido del elemento 4" },
+          { key: "Elemento 5", content: "Este es el contenido del elemento 5" },
+          { key: "Elemento 6", content: "Este es el contenido del elemento 6" },
+          { key: "Elemento 7", content: "Este es el contenido del elemento 7" },
+          { key: "Elemento 8", content: "Este es el contenido del elemento 8" },
+          { key: "Elemento 9", content: "Este es el contenido del elemento 9" },
+          {
+            key: "Elemento 10",
+            content: "Este es el contenido del elemento 10",
+          },
+        ]}
+        renderItem={({ item }) => (
+          <CardComponent
+            title={item.key}
+            content={item.content}
+            navigation={ navigation }
+          ></CardComponent>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
     )
 }
